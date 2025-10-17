281f57c13f8068e259cf59543737ab8851fba6eb
IMALDOWSKI <hikmal393@gmail.com>
2025-10-17 09:50:58 +0700

    While & Do While

--- looping.php
+++ looping.php
@@ -1,7 +1,48 @@
 <?php
 ### FOR ###
+echo "FOR";
+echo "<br>";
+echo "<br>";
+
 for($i = 10; $i >= 1; $i--){
     echo "Mahasiswa ke-$i <br>";
 }
+echo "<hr>";
+
+### WHILE ###
+echo "WHILE";
+echo "<br>";
+echo "<br>";
+
+$awal = 1;
+while($awal <= 10){
+    echo "Mahasiswa ke - $awal <br>";
+    $awal++;
+}
+echo "<hr>";
+
+### DO WHILE ###
+echo "DO WHILE";
+echo "<br>";
+echo "<br>";
+
+$dw = 1;
+do{
+    echo "Mahasiswa ke - $dw <br>";
+    $dw++;
+}while($dw <= 10);
+echo "<hr>";
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
